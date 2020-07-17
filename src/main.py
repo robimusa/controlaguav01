37472a500dc84df04d132e1aba6a7006f8306d09
Roberto MS <robertomunizsalom@gmail.com>
2020-07-17 18:09:27 +0200

    Inicio del Main.py (Carga de Librerias, Conexión BD, Comprobar que el servidor funciona) y creación un index.html provisional para comprobar lectura en el navegador. Funciona!

---
+++ src/main.py
@@ -0,0 +1,26 @@
+# PROYECTO CONTROL CONSUMO DE AGUA
+
+# IMPORTACIÓN DE LIBRERIAS
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+import os
+
+# CONEXION MONGODB + BASE DE DATOS + COLECCION
+URL_MONGO = 'mongodb+srv://Invitado:Guest5050@cluster0-3lrrr.mongodb.net/test?retryWrites=true&w=majority'
+cliente = MongoClient(URL_MONGO, ssl_cert_reqs=False)
+db = cliente['primeraBD']
+coleccion = db['consumoagua']
+
+app = Flask(__name__)
+
+# RUTAS
+@app.route('/', methods=["GET","POST"])
+def home():
+    return render_template ('index.html')
+
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT",5000))
+    app.run(host='0.0.0.0',port=port,debug=True)
