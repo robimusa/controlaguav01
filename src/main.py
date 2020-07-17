2a35123774143ef2d680528116453a30f9f4a5b4
Roberto MS <robertomunizsalom@gmail.com>
2020-07-17 21:31:20 +0200

    Añadido funcionalidades Nuevo, Listar, Borrar, Editar. Editar no funciona. Revisar

--- src/main.py
+++ src/main.py
@@ -15,12 +15,74 @@ coleccion = db['consumoagua']
 app = Flask(__name__)
 
 # RUTAS
-@app.route('/', methods=["GET","POST"])
+@app.route('/', methods=["GET", "POST"])
 def home():
-    return render_template ('index.html')
+    return render_template('index.html')
 
+
+@app.route("/nuevo")
+def nuevoConsumo():
+    return render_template('nuevo.html')
+
+
+@app.route("/entrar-nuevo", methods=['GET', 'POST'])
+def entrarNuevoConsumo():
+    if request.method == 'POST':
+        fecha = request.form['fecha']
+        manana = request.form['manana']
+        mediodia = request.form['mediodia']
+        tarde = request.form['tarde']
+        noche = request.form['noche']
+        nuevoregistro = {"fecha": fecha, "manana": manana,
+                         "mediodia": mediodia, "tarde": tarde, "noche": noche}
+        coleccion.insert_one(nuevoregistro)
+        return redirect(url_for('nuevoConsumo'))
+    return redirect(url_for('nuevoConsumo'))
+
+
+@app.route("/mostrar-todos")
+def mostrarTodos():
+    registros = coleccion.find()
+    tituloDelListado = "Listado de todos los consumos"
+    return render_template('mostrar-todos.html', datos=registros, tituloDelListado=tituloDelListado)
+
+
+@app.route("/editar/<id>")
+def editarConsumo(id):
+    buscarPorId = {"_id": ObjectId(id)}
+    registro = coleccion.find_one(buscarPorId)
+    return render_template('/editar.html', datos=registro)
+
+
+@app.route("/editar-confirmado/<id>", methods=['GET', 'POST'])
+def actualizarConsumo(id):
+    if request.method == 'POST':
+        fecha = request.form['fecha']
+        manana = request.form['manana']
+        mediodia = request.form['mediodia']
+        tarde = request.form['tarde']
+        noche = request.form['noche']
+        registroActualizado = {"fecha": fecha, "manana": manana,
+                               "mediodia": mediodia, "tarde": tarde, "noche": noche}
+        buscarPorId = {"_id": ObjectId(id)}
+        coleccion.update_one(buscarPorId, {"$set": registroActualizado})
+        return redirect(url_for('mostrarTodos'))
+    return redirect(url_for('editarConsumo'))
+
+@app.route("/borrar/<id>")
+def borrarConsumo(id):
+    buscarPorId = {"_id": ObjectId(id)}
+    registro = coleccion.find_one(buscarPorId)
+    return render_template('/borrar.html', datos=registro)
+
+
+@app.route("/confirmar-borrado/<string:id>")
+def confirmadoBorrado(id):
+    buscarPorId = {"_id": ObjectId(id)}
+    coleccion.delete_one(buscarPorId)
+    return redirect(url_for('mostrarTodos'))
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT",4000))
-    app.run(host='0.0.0.0',port=port,debug=True)
+    port = int(os.environ.get("PORT", 4000))
+    app.run(host='0.0.0.0', port=port, debug=True)
